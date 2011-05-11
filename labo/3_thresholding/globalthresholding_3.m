3e1db18e4b08f946255e26f1bdec7b181c17120c
Ruben Schollaert <ruben.schollaert@telenet.be>
2011-05-11 14:36:51 +0200

    labos

--- labo/3_thresholding/globalthresholding_3.m
+++ labo/3_thresholding/globalthresholding_3.m
@@ -6,13 +6,13 @@ conv_beeld = imfilter(image,fspecial('average', [6, 6]), 'replicate');
 %Trek het origineel van het geconvolueerde beeld af.
 verschil = conv_beeld - image;
 
-
 %Segmenteer het verschilbeeld met drempelwaarde 0.
 cte=4;
 binary=im2bw(verschil-cte,0);
 
 % Waarom resulteert deze aanpassing in een verbetering van het resultaat?
-% ???
+%   im2bw gaat alle pixels in de input die groter zijn dan 0 vervangen door 1
+%   zo krijgen we dus meer ingekleurd zonder het verschil met die constante.
 
 figure,imshow(not(binary));
 end
